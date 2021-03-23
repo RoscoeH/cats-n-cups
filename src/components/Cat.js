2eefdf99ef96b22704963c6001b73d182ca03cf7
Roscoe Hart <roscoewh@gmail.com>
2021-03-23 19:10:41 +1000

    Fix body color animation

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -79,19 +79,9 @@ const Body = () => (
 const Tail = ({ visible }) => (
   <AnimatePresence>
     {visible && (
-      // <motion.path
-      //   key="child"
-      //   initial={{ pathLength: 0 }}
-      //   animate={{ pathLength: 1 }}
-      //   exit={{ pathLength: 0 }}
-      //   d="M5.02158 4.86789C4.48268 5.44271 4 6.42818 4 8.00004C4 9.10461 3.10457 10 2 10C0.89543 10 0 9.10461 0 8.00004C0 5.57187 0.767316 3.55732 2.10342 2.13213C3.42996 0.71715 5.21338 -8.34464e-07 7 0C8.78662 8.34466e-07 10.57 0.717154 11.8966 2.13214C13.2327 3.55733 14 5.57188 14 8.00005C14 9.10462 13.1046 10.0001 12 10.0001C10.8954 10.0001 10 9.10462 10 8.00005C10 6.42819 9.51732 5.44272 8.97842 4.86789C8.42996 4.28286 7.71338 4 7 4C6.28662 4 5.57004 4.28286 5.02158 4.86789Z"
-      //   transform="translate(28 8)"
-      // />
-
       <motion.path
         key="child"
         d="M2 7C2 4.23858 4.23858 2 7 2C9.76142 2 12 4.23858 12 7"
-        // d="M12 7C12 4.23858 9.76142 12 7 2C4.23858 2 2 4.23858 2 7"
         stroke="white"
         strokeWidth="4"
         strokeLinecap="round"
@@ -162,6 +152,7 @@ const Face = ({ id, color, mad }) => {
 
 export const Cat = ({ id, color, mad, legs, tail, cupped, size }) => {
   useEffect(() => console.log("mount cat"), []);
+  const bodyColor = theme.colors.cat[color || "blue"];
   return (
     <svg width={size || 128} height={size || 128} viewBox="0 0 56 56">
       <mask id={`catMask${id}`}>
@@ -177,8 +168,9 @@ export const Cat = ({ id, color, mad, legs, tail, cupped, size }) => {
           y="0"
           width="56"
           height="56"
+          initial={{ fill: bodyColor }}
           animate={{
-            fill: mad ? theme.colors.angry : theme.colors.cat[color || "blue"],
+            fill: mad ? theme.colors.angry : bodyColor,
           }}
         />
       </g>
