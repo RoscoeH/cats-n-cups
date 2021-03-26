e57048cc751ebae36e7099eab5c89fedb8af0f07
Roscoe Hart <roscoewh@gmail.com>
2021-03-26 15:21:50 +1000

    Refactor WinModal

--- src/components/WinModal.js
+++ src/components/WinModal.js
@@ -26,6 +26,7 @@ const variants = {
     opacity: 1,
     y: 0,
     transition: {
+      delay: 0.5,
       duration: 0.5,
       ease: "easeOut",
     },
@@ -55,79 +56,80 @@ const WinModal = ({ time, moves, stars }) => {
     setProgress(newProgress);
     setData(newProgress);
 
-    const timeout = setTimeout(sequenceAnimation, 500);
+    const timeout = setTimeout(sequenceAnimation, 3000);
     return () => clearTimeout(timeout);
   });
 
   return (
-    <div
+    <motion.div
       sx={{
         display: "inline-block",
         borderRadius: 32,
         bg: "light",
-        color: "shadow",
-        boxShadow: "0 0 16px",
-        py: 3,
-        px: 4,
         zIndex: 100,
+        textAlign: "center",
+        position: "absolute",
+        top: 0,
+        width: "100%",
+        height: "100%",
+        pt: 5,
+      }}
+      initial={{ opacity: 0, y: 128 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          delay: 2,
+        },
       }}
     >
-      <div
-        sx={{
-          color: "dark",
-          textAlign: "center",
-          "& > *:not(:last-child)": {
-            mb: 2,
-          },
-        }}
+      <Styled.h2 sx={{ mb: 4 }}>You Win!</Styled.h2>
+      <table sx={{ mx: "auto", mb: 2 }}>
+        <tbody>
+          <motion.tr
+            variants={variants}
+            initial="hidden"
+            animate={timeControls}
+          >
+            <StatHeader>time</StatHeader>
+            <StatData>{formatTime(time)}</StatData>
+          </motion.tr>
+          <motion.tr
+            variants={variants}
+            initial="hidden"
+            animate={movesControls}
+          >
+            <StatHeader>moves</StatHeader>
+            <StatData>{moves}</StatData>
+          </motion.tr>
+        </tbody>
+      </table>
+      <motion.div
+        sx={{ mb: 3 }}
+        variants={variants}
+        initial="hidden"
+        animate={starsControls}
       >
-        <Styled.h2>You Win!</Styled.h2>
-        <table sx={{ margin: "0 auto" }}>
-          <tbody>
-            <motion.tr
-              variants={variants}
-              initial="hidden"
-              animate={timeControls}
-            >
-              <StatHeader>time</StatHeader>
-              <StatData>{formatTime(time)}</StatData>
-            </motion.tr>
-            <motion.tr
-              variants={variants}
-              initial="hidden"
-              animate={movesControls}
-            >
-              <StatHeader>moves</StatHeader>
-              <StatData>{moves}</StatData>
-            </motion.tr>
-          </tbody>
-        </table>
-        <motion.div
-          variants={variants}
-          initial="hidden"
-          animate={starsControls}
-        >
-          <StarBar stars={stars} />
-        </motion.div>
-        <motion.div
-          variants={variants}
-          initial="hidden"
-          animate={buttonsControls}
-        >
-          <Button large sx={{ mb: 2 }}>
-            Next
+        <StarBar stars={stars} />
+      </motion.div>
+      <motion.div
+        variants={variants}
+        initial="hidden"
+        animate={buttonsControls}
+      >
+        <Button large sx={{ mb: 3 }}>
+          Next
+        </Button>
+        <div>
+          <Button secondary onClick={() => history.push("/levels")}>
+            Levels
           </Button>
-          <div>
-            <Button secondary onClick={() => history.push("/levels")}>
-              Levels
-            </Button>
-            <Button secondary onClick={() => history.go(0)}>
-              Retry
-            </Button>
-          </div>
-        </motion.div>
-      </div>
-    </div>
+          <Button secondary onClick={() => history.go(0)}>
+            Retry
+          </Button>
+        </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- src/pages/Play.js
+++ src/pages/Play.js
@@ -19,26 +19,19 @@ export const Play = ({ game }) => {
   const [solved] = useObservable(game.solved);
   const cellSize = Math.min((width || MAX_SIZE) / game.cols, MAX_SIZE);
 
-  return solved ? (
-    <div
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        pt: 5,
-      }}
-    >
-      <WinModal
-        time={game.time.get()}
-        moves={game.moves.get()}
-        stars={game.stars}
-      />
-    </div>
-  ) : (
+  return (
     <div ref={ref}>
       <CustomDragLayer size={cellSize} />
       <Header />
       <Grid size={cellSize} />
       <Dock size={cellSize} />
+      {solved && (
+        <WinModal
+          time={game.time.get()}
+          moves={game.moves.get()}
+          stars={game.stars}
+        />
+      )}
     </div>
   );
 };
