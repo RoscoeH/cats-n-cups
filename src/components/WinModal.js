b9bc40ea945696944885c50f45f4ee85ad3e4f3c
Roscoe Hart <roscoewh@gmail.com>
2021-03-27 13:07:20 +1000

    Reset game

--- src/components/WinModal.js
+++ src/components/WinModal.js
@@ -34,7 +34,7 @@ const variants = {
 };
 
 const WinModal = ({ time, moves, stars }) => {
-  const [game, load] = useGame();
+  const [game] = useGame();
   const [progress, setProgress] = useProgress();
   const history = useHistory();
   const [, setData] = useStorage("progress");
@@ -124,7 +124,7 @@ const WinModal = ({ time, moves, stars }) => {
           <Button secondary onClick={() => history.push("/levels")}>
             Levels
           </Button>
-          <Button secondary onClick={() => load()}>
+          <Button secondary onClick={() => history.go(0)}>
             Retry
           </Button>
         </div>
